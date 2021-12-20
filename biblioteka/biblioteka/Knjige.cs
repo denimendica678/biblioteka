39312b4173708b7a5d582e872668ea2ef0e4986a
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-12-20 09:48:50 +0100

    Add files via upload

---
+++ biblioteka/biblioteka/Knjige.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteka
+{
+    internal class Knjige
+    {
+        int isbn;
+        string naziv, autor;
+
+        public Knjige(int isbn, string naziv, string autor)
+        {
+            this.isbn = isbn;
+            this.naziv = naziv;
+            this.autor = autor;
+        }
+
+        public int Isbn { get => isbn; set => isbn = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+        public string Autor { get => autor; set => autor = value; }
+    }
+}
---
+++ biblioteka/biblioteka/Posudba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblioteka
+{
+    internal class Posudba
+    {
+        DateTime datum_p;
+        DateTime datum_v;
+        int id_knjige, id_korisnici;
+
+        public Posudba(DateTime datum_p, DateTime datum_v, int id_knjige, int id_korisnici)
+        {
+            this.datum_p = datum_p;
+            this.datum_v = datum_v;
+            this.id_knjige = id_knjige;
+            this.id_korisnici = id_korisnici;
+        }
+
+        public DateTime Datum_p { get => datum_p; set => datum_p = value; }
+        public DateTime Datum_v { get => datum_v; set => datum_v = value; }
+        public int Id_knjige { get => id_knjige; set => id_knjige = value; }
+        public int Id_korisnici { get => id_korisnici; set => id_korisnici = value; }
+    }
+}
